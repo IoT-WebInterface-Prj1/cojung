458f537d7d7e3a9cc60ca1a7a998c7e216728eb9
yannJu <yann_ju@naver.com>
2023-03-07 15:11:03 +0900

    views.py 추가 및 분리

---
+++ cojung/question_view.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+from django.db.models import Q, Count
+from django.core.paginator import Paginator
+
+# ========
+# Question Line
+# ========
+def question(request):
+    """
+    코딩의 정석 "Question" 목록 출력
+    """
+    questionLst = Question.objects.order_by('-create_date')
+    # context = {'questionLst' : questionLst}
+    
+    # ==============
+    # 페이징 처리
+    # ==============
+    page = request.GET.get('page', 1)
+    
+    paginator = Paginator(questionLst, 2)
+    pageObj = paginator.get_page(page)
+    
+    # 페이징 기준으로 context 변수 정의
+    context = {
+        'questionLst' : pageObj,
+    }
+    
+    return render(request, 'cojung/question_list.html', context)
+
+def question_detail(request, question_id):
+    """
+    코딩의 정석 "Question" 게시글 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question' : question
+    }
+    
+    return render(request, 'cojung/question_detail.html', context)
