24c14050d93eec6c2925b89315afefe33feba620
yannJu <yann_ju@naver.com>
2023-03-07 15:10:05 +0900

    url 변경

--- cojung/urls.py
+++ cojung/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'cojung'
 
 urlpatterns = [
+    #Problem Line --
     path('', views.index, name='index'),
+    # path('<int:problem_id>', views.problem_detail, name='problem_detail'),
+    #Question Line --
+    path('question/', views.question, name = 'question'),
+    path('question/<int:question_id>', views.question_detail, name = 'question_detail'),
 ]
