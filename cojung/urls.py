3da44f4d6c9c414ade3ed5dede0fbab62f385279
yannJu <yann_ju@naver.com>
2023-03-14 14:31:13 +0900

    comment_수정 삭제 기능 추가

--- cojung/urls.py
+++ cojung/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     
     #Answer Line --
     path('answer/create/<int:question_id>', answer_views.answer_create, name = 'answer_create'),
+    path('answer/modify/<int:answer_id>', answer_views.answer_modify, name = 'answer_modify'),
+    path('answer/delete/<int:answer_id>', answer_views.answer_delete, name = 'answer_delete'),
 
     # 문제 업로드 / 질문 작성
     path('createpost/', post_views.create_post, name='create_post'),
@@ -28,6 +30,8 @@ urlpatterns = [
     
     #Comment Line --
     path('comment/create/<int:answer_id>', comment_views.comment_answer_create, name = 'comment_answer_create'),
+    path('comment/modify/<int:comment_id>', comment_views.comment_answer_modify, name = 'comment_answer_modify'),
+    path('comment/delete/<int:comment_id>', comment_views.comment_answer_delete, name = 'comment_answer_delete'),
 
     #풀이 생성 ,댓글
     path('resolve/detail/<int:problem_id>/',resolve_views.resolve_detail,name='resolve_detail'),
